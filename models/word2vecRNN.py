f674af4ba94abe94aea88f5efcf1d3ef6dff1958
Tim Rietveld <timr2808@gmail.com>
2019-05-06 13:08:05 +0200

    added word2vec model

---
+++ models/word2vecRNN.py
@@ -0,0 +1,55 @@
+from preprocess.word2vec import convert_to_keras_embeddings
+from preprocess.parser import get_data_as_lists
+from keras import Sequential
+from keras.layers import SimpleRNN, Dense
+from keras.optimizers import Adam
+from keras.preprocessing.text import text_to_word_sequence, one_hot
+
+
+model = Sequential()
+
+
+x = get_data_as_lists()[0][0]
+y = get_data_as_lists()[0][1]
+
+x_test = get_data_as_lists()[1][0]
+y_test = get_data_as_lists()[1][1]
+
+
+simple_rnn_params = {"units": 10, "activation": 'tanh', "use_bias": True, "kernel_initializer": 'glorot_uniform',
+                     "recurrent_initializer": 'orthogonal', "bias_initializer": 'zeros', "kernel_regularizer": None,
+                     "recurrent_regularizer": None, "bias_regularizer": None, "activity_regularizer": None,
+                     "kernel_constraint": None, "recurrent_constraint": None, "bias_constraint": None, "dropout": 0.0,
+                     "recurrent_dropout": 0.0, "return_sequences": True, "return_state": False, "go_backwards": False,
+                     "stateful": False, "unroll": False
+                     }
+
+dense_parameters = {"units": 50000, "activation": None, "use_bias": True, "kernel_initializer": 'glorot_uniform',
+                    "bias_initializer": 'zeros', "kernel_regularizer": None, "bias_regularizer": None,
+                    "activity_regularizer": None, "kernel_constraint": None, "bias_constraint": None}
+
+adam_params = {"lr": 0.001, "beta_1": 0.9, "beta_2": 0.999, "epsilon": None, "decay": 0.0, "amsgrad": False}
+
+train_params = {"epochs": 1, "batch_size": 8}
+
+
+model.add(convert_to_keras_embeddings(x, load_from_file="../embeddings/train_comment.model"))
+model.add(SimpleRNN(**simple_rnn_params))
+model.add(Dense(**dense_parameters))
+
+adam = Adam(**adam_params)
+model.compile(loss='mean_squared_error', optimizer=adam)
+
+
+
+
+tokenized_x = [text_to_word_sequence(sequence) for sequence in x]
+# tokenized_y = [text_to_word_sequence(sequence) for sequence in y]
+
+
+tokenized_y = [one_hot(sequence, 50000) for sequence in y]
+
+model.fit(tokenized_x, tokenized_y, **train_params)
+
+score = model.evaluate(x_test, y_test, batch_size=32)
+print(score)
--- preprocess/word2vec.py
+++ preprocess/word2vec.py
@@ -2,19 +2,19 @@ from preprocess.parser import get_data_as_lists
 from gensim.utils import simple_preprocess
 import gensim
 
-def read_data(input_strings):
+def _tokenize_data(input_strings):
     for line in input_strings:
         yield simple_preprocess(line)
 
 
 
-def create_embeddings(input_strings):
-    documents = list(read_data(input_strings))
+def _create_embeddings(input_strings):
+    documents = list(_tokenize_data(input_strings))
 
     model = gensim.models.Word2Vec(
         documents,
-        size=3000,
-        window=2,
+        size=150,
+        window=10,
         min_count=1,
         workers=10)
     model.train(documents, total_examples=len(documents), epochs=10)
@@ -22,5 +22,27 @@ def create_embeddings(input_strings):
 
 
 
-code_data = get_data_as_lists()[0][0]
-create_embeddings(code_data)
+def create_code_embedding(save=True):
+    code_data = get_data_as_lists()[0][0]
+    embedding = _create_embeddings(code_data)
+    if(save):
+        embedding.save("../embeddings/train_code.model")
+    return embedding
+
+
+def create_comment_embedding(save=True):
+    comment_data = get_data_as_lists()[0][1]
+    embedding = _create_embeddings(comment_data)
+    if(save):
+        embedding.save("../embeddings/train_comment.model")
+    return embedding
+
+
+def convert_to_keras_embeddings(corpus, wv=None, load_from_file=""):
+    if(len(load_from_file)>0):
+        wv = gensim.models.KeyedVectors.load(load_from_file, mmap='r')
+    elif(wv is None):
+        wv = _create_embeddings(corpus)
+        print("created embedding for corpus")
+
+    return wv.get_keras_embedding(False)
